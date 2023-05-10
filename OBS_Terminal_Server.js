69d235a05c0c30719617045185c5885cd6e9e4d6
Blabzillaweasel <connormcgeek@gmail.com>
2023-05-10 18:00:12 +1200

    Added functionality
    Added looping function
    Added stop function
    Added clear function

--- OBS_Terminal_Server.js
+++ OBS_Terminal_Server.js
@@ -10,20 +10,32 @@ const clients = {
 wss.on('connection', (ws) => {
   console.log('Client connected');
 
-  ws.on('message', (message) => {
-    const data = JSON.parse(message);
+ws.on('message', (message) => {
+  const data = JSON.parse(message);
 
-    if (data.type === 'register') {
-      clients[data.role] = ws;
-      console.log(`${data.role} registered`);
-    } else if (data.type === 'text') {
-      console.log('Received text:', data.text);
+  if (data.type === 'register') {
+    clients[data.role] = ws;
+    console.log(`${data.role} registered`);
+  } else if (data.type === 'text') {
+    console.log('Received text:', data.text);
 
-      if (clients.display) {
-        clients.display.send(JSON.stringify({ type: 'text', text: data.text }));
-      }
+    if (clients.display) {
+      clients.display.send(JSON.stringify({ type: 'text', text: data.text, loop: data.loop }));
     }
-  });
+  } else if (data.type === 'stop_loop') {
+    console.log('Stopping loop');
+
+    if (clients.display) {
+      clients.display.send(JSON.stringify({ type: 'stop_loop' }));
+    }
+  } else if (data.type === 'clear_display') {
+    console.log('Clearing display');
+
+    if (clients.display) {
+      clients.display.send(JSON.stringify({ type: 'clear_display' }));
+    }
+  }
+});
 
   ws.on('close', () => {
     console.log('Client disconnected');
